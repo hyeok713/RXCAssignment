c4f73a7521a581bf66fbb657d2e7913d0235380a
HYEOKBEOM <part1shb@gmail.com>
2023-06-29 17:42:52 +0900

    chore

--- app/src/main/java/com/hyeokbeom/rxcassignment/MainActivity.kt
+++ app/src/main/java/com/hyeokbeom/rxcassignment/MainActivity.kt
@@ -3,7 +3,7 @@ package com.hyeokbeom.rxcassignment
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.Text
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -14,6 +14,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             RXCAssignmentTheme {
+
             }
         }
     }
