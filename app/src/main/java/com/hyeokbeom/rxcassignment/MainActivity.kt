8ac32a249343b0c0ee224666c63923271a45973e
HYEOKBEOM <part1shb@gmail.com>
2023-06-29 19:08:06 +0900

    feat: mainList api call test

--- app/src/main/java/com/hyeokbeom/rxcassignment/MainActivity.kt
+++ app/src/main/java/com/hyeokbeom/rxcassignment/MainActivity.kt
@@ -3,17 +3,23 @@ package com.hyeokbeom.rxcassignment
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.hyeokbeom.rxcassignment.ui.theme.RXCAssignmentTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private val viewModel: MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             RXCAssignmentTheme {
+
             }
         }
     }
--- app/src/main/java/com/hyeokbeom/rxcassignment/MainViewModel.kt
+++ app/src/main/java/com/hyeokbeom/rxcassignment/MainViewModel.kt
@@ -1,10 +1,57 @@
 package com.hyeokbeom.rxcassignment
 
 import androidx.lifecycle.ViewModel
+import com.hyeokbeom.domain.usecase.MainListUseCase
+import com.hyeokbeom.rxcassignment.ext.launchOnIO
+import com.hyeokbeom.shared.Log
+import com.hyeokbeom.shared.executeResult
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
-): ViewModel() {
+    private val mainListUseCase: MainListUseCase
+): ViewModel(), MainViewIntent {
+    fun haha() {
+        println("하하!")
+    }
+    val mainViewState = MutableStateFlow<Int>(0)
+    init {
+        launchOnIO {
+            mainListUseCase().executeResult(
+                onSuccess = {
+                    Log.e(it.toString())
+                },
+                onFailure = {
+                    Log.e("실패했음")
+                }
+            )
+        }
+    }
+
+    override fun a() {
+    }
+
+    override fun b() {
+    }
+}
+
+/**
+ * MainViewIntent
+ * [메인 뷰 Intent 관리]
+ */
+interface MainViewIntent {
+    fun a()
+    fun b()
+}
+
+/**
+ * MainViewState
+ * [메인 뷰 State 관리]
+ */
+sealed class MainViewState {
+    object IsLoading: MainViewState()   // 데이터 로딩 상태
+    object Error: MainViewState()       // 데이터 로딩 실패
+    data class MainList(val list: List<Int>): MainViewState()   // 데이터 로딩 성공시 with list
 }
