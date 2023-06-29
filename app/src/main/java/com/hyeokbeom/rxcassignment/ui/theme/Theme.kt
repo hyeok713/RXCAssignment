450adc77f75e53388270021d1a127071b6ebd968
HYEOKBEOM <part1shb@gmail.com>
2023-06-29 18:42:37 +0900

    edit: set theme

--- app/src/main/java/com/hyeokbeom/rxcassignment/ui/theme/Theme.kt
+++ app/src/main/java/com/hyeokbeom/rxcassignment/ui/theme/Theme.kt
@@ -10,21 +10,22 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Color.Black,
+    secondary = Color.Black,
+    tertiary = Color.Black
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = Color.White,
+    secondary = Color.White,
+    tertiary = Color.White
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -58,7 +59,8 @@ fun RXCAssignmentTheme(
         SideEffect {
             val window = (view.context as Activity).window
             window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
         }
     }
 
